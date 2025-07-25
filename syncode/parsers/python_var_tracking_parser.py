c0f19da1c0243a6a92e25cbbed99698099dd9507
5ila5 <5ila5@users.noreply.github.com>
2025-07-25 18:54:02 +0200

    reformat python_var_tracking_parser.py

--- syncode/parsers/python_var_tracking_parser.py
+++ syncode/parsers/python_var_tracking_parser.py
@@ -1,11 +1,9 @@
-
 import logging
 from typing import Iterable, Tuple
 
 import regex
 from syncode.larkm import Token
 import syncode.larkm as lark
-
 
 
 from syncode.larkm.parsers.lalr_parser_state import ParserState
@@ -15,26 +13,29 @@ from syncode.larkm.tree import Tree
 
 logger = logging.getLogger(__name__)
 
+
 class PythonVarTrackingIncrementalParser(IGParser):
     """
     This class implements an incremental parser for Python code, tracking variable defintions and uses.
     """
 
-    def __init__(self, base_parser, indenter, partial_code=None,**kwargs):
+    def __init__(self, base_parser, indenter, partial_code=None, **kwargs):
         super().__init__(base_parser, ignore_whitespace=False)
 
-        if partial_code is not None: # extract indentation type from partial code
+        if partial_code is not None:  # extract indentation type from partial code
             indenter.tab_len = self._get_indentation(partial_code)
         self.tab_len = indenter.tab_len
 
         self._defined_vars: set[Token] = set()
 
     def _get_indentation(self, partial_code) -> int:
-        m = regex.match(r"(.*?):(.*?)\n(.*?)(?![ \t])", partial_code, flags=regex.DOTALL)
+        m = regex.match(
+            r"(.*?):(.*?)\n(.*?)(?![ \t])", partial_code, flags=regex.DOTALL
+        )
         indent_type = m.group(3)
-        tab_len = 4 # Default tab length
-        if '\t' not in indent_type: # that means we are using spaces for indentation
-            tab_len = indent_type.count(' ')
+        tab_len = 4  # Default tab length
+        if "\t" not in indent_type:  # that means we are using spaces for indentation
+            tab_len = indent_type.count(" ")
         return tab_len
 
     @property
@@ -43,8 +44,10 @@ class PythonVarTrackingIncrementalParser(IGParser):
         Returns a copy of the list of defined variables.
         """
         return [v for v in self._defined_vars]
-    
-    def get_vars(self, values: Iterable[Token | Tree], next_name_type: str | None = None) -> list[tuple[str, str]]:
+
+    def get_vars(
+        self, values: Iterable[Token | Tree], next_name_type: str | None = None
+    ) -> list[tuple[str, str]]:
         if next_name_type is None:
             next_name_type = "USE"
         vars: list[tuple[str, str]] = []
@@ -55,20 +58,30 @@ class PythonVarTrackingIncrementalParser(IGParser):
                     # if it.value == "name_define":
                     #     next_name_type = "USE"
                     # el
-                    if it.value in ("function_name", "param_var_name", "except_exception_name", "with_as_expr", "def_expr_list", "assign_expr", "assign_exprs"):
+                    if it.value in (
+                        "function_name",
+                        "param_var_name",
+                        "except_exception_name",
+                        "with_as_expr",
+                        "def_expr_list",
+                        "assign_expr",
+                        "assign_exprs",
+                    ):
                         next_name_type = "DEFINE"
                         print("found Rule:", it.value)
                 elif it.type == "NAME":
                     vars.append((it.value, next_name_type))
-                    
-            
+
             elif isinstance(it, Tree):
                 data = it.data
-                
+
                 if isinstance(data, Token):
                     vars += self.get_vars([data, *it.children], next_name_type)
                 else:
-                    if isinstance(data, str) and data in ("assign_exprs", "assign_expr"):
+                    if isinstance(data, str) and data in (
+                        "assign_exprs",
+                        "assign_expr",
+                    ):
                         next_name_type = "DEFINE"
                     vars += self.get_vars(it.children, next_name_type)
 
@@ -87,18 +100,20 @@ class PythonVarTrackingIncrementalParser(IGParser):
 
         self._defined_vars.clear()
 
-        vars:list[tuple[str, str]] = []
+        vars: list[tuple[str, str]] = []
 
         try:
             while lexer_state.line_ctr.char_pos < len(lexer_state.text):
                 # PostLexConnector -> BasicLexer
                 blexer = interactive.lexer_thread.lexer.lexer
-                
+
                 token = blexer.next_token(lexer_state)
                 self.lexer_pos = lexer_state.line_ctr.char_pos
 
                 # print(self.cur_pos_to_parser_state)
-                current_tokens = lexer_tokens[:len(lexer_tokens)]  # All tokens processed so far
+                current_tokens = lexer_tokens[
+                    : len(lexer_tokens)
+                ]  # All tokens processed so far
                 key = self._get_hash(current_tokens)
 
                 if key in self.cur_pos_to_parser_state:
@@ -106,9 +121,11 @@ class PythonVarTrackingIncrementalParser(IGParser):
                     parser_state = stored_state[1]
                     if isinstance(parser_state, ParserState):
                         print(parser_state.value_stack)
-                        vars = self.get_vars(parser_state.value_stack)          
+                        vars = self.get_vars(parser_state.value_stack)
                     else:
-                        print(f"Warning: Expected ParserState, got {type(parser_state)} for key {key}")
+                        print(
+                            f"Warning: Expected ParserState, got {type(parser_state)} for key {key}"
+                        )
                 else:
                     print(f"No stored state found for key {key}")
 
@@ -121,17 +138,16 @@ class PythonVarTrackingIncrementalParser(IGParser):
                 else:
                     lexer_tokens.append(token)
                 if token.type in indenter.OPEN_PAREN_types:
-                        indenter.paren_level += 1
+                    indenter.paren_level += 1
                 elif token.type in indenter.CLOSE_PAREN_types:
-                        indenter.paren_level -= 1
-                        assert indenter.paren_level >= 0
-        except lark.exceptions.UnexpectedCharacters as e: 
+                    indenter.paren_level -= 1
+                    assert indenter.paren_level >= 0
+        except lark.exceptions.UnexpectedCharacters as e:
             lexing_incomplete = True
-            pass # This may happen when the partial code has an ignore terminal
+            pass  # This may happen when the partial code has an ignore terminal
         except EOFError as e:
             pass
-            
+
         print("vars:", vars)
 
         return lexer_tokens, lexing_incomplete
-
